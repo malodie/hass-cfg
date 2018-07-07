88450a6662153d7b077894ed37c364928bbffa4c
malodie <mallory.quaintance@gmail.com>
2018-07-06 21:09:55 -0400

    something

--- apps/scenes.py
+++ apps/scenes.py
@@ -17,6 +17,10 @@ class Scenes(hass.Hass):
       self.listen_state(self.scene_change, self.args["selector"])
 
   def scene_change(self, entity, attribute, old, new):
+      self.log(select)
+      self.log(loc)
+      self.log(self.args["selector"])
+      self.log(self.args["location"])
       option = self.get_state(self.args["selector"])
       self.log("Changing the light temp.")
       self.call_service("hue/hue_activate_scene", group_name = self.args["location"], scene_name = option)
