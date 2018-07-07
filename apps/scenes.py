76cd5618c4a5a84dc2c06bc777a88111972fe3b5
malodie <mallory.quaintance@gmail.com>
2018-07-06 20:16:35 -0400

    something

--- apps/scenes.py
+++ apps/scenes.py
@@ -10,7 +10,10 @@ class Scenes(hass.Hass):
 
   def initialize(self):
       self.listen_state(self.scene_change, "input_select.office_scene")
+      self.log("At least it started.")
 
   def scene_change(self, entity, attribute, old, new, kwargs):
       option = self.get_state("input_select.office_scene")
+      self.log(option)
+      self.log("Okay, we also got the change")
       self.call_service(self, hue.hue_activate_scene, group_name = "Office", scene_name = option)
