d26bb41390a239149daa422dc60ce2ef5de3033b
malodie <mallory.quaintance@gmail.com>
2018-07-06 21:34:15 -0400

    fjskljdfkl

--- apps/scenes.py
+++ apps/scenes.py
@@ -9,12 +9,9 @@ import appdaemon.plugins.hass.hassapi as hass
 class Scenes(hass.Hass):
 
   def initialize(self):
-      self.selector = self.args["selector"]
-      self.location = self.args["location"]
       self.listen_state(self.scene_change, "input_select.office_scene")
 
   def scene_change(self, entity, attribute, old, new, kwargs):
-      self.log(selector)
       option = self.get_state("input_select.office_scene")
       self.log("Changing the light temp.")
       self.call_service("hue/hue_activate_scene", group_name = "Office", scene_name = option)
