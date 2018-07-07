b3a9bbf4bd437ccaec87b8b5aa681ed03c897175
malodie <mallory.quaintance@gmail.com>
2018-07-06 21:53:39 -0400

    fjskljdfkl

--- apps/scenes.py
+++ apps/scenes.py
@@ -11,7 +11,7 @@ class Scenes(hass.Hass):
   def initialize(self):
       self.location = self.args['location']
       self.selector = self.args['selector']
-      self.listen_state(self.scene_change, "input_select.office_scene", self.location, self.selector)
+      self.listen_state(self.scene_change, "input_select.office_scene")
 
   def scene_change(self, entity, attribute, old, new, kwargs):
       option = self.get_state("input_select.office_scene")
