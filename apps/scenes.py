a93a7e9a253a5572110012b0beda08fa13b77ed2
malodie <mallory.quaintance@gmail.com>
2018-07-06 21:55:52 -0400

    fjskljdfkl

--- apps/scenes.py
+++ apps/scenes.py
@@ -9,9 +9,9 @@ import appdaemon.plugins.hass.hassapi as hass
 class Scenes(hass.Hass):
 
   def initialize(self):
-      self.location = self.args['location']
-      self.selector = self.args['selector']
-      self.listen_state(self.scene_change, "input_select.office_scene")
+      location = self.args['location']
+      selector = self.args['selector']
+      self.listen_state(self.scene_change, selector)
 
   def scene_change(self, entity, attribute, old, new, kwargs):
       option = self.get_state("input_select.office_scene")
